4331b7b87f8564a0b387652cf78221d2925dd969
LAMXINLE <xlam0003@student.monash.edu>
2023-02-16 22:50:09 +0800

    add in updateIPFSMetadata util

--- yakiniku-frontend/src/utils/pinata.js
+++ yakiniku-frontend/src/utils/pinata.js
@@ -31,6 +31,36 @@ export const uploadJSONToIPFS = async(JSONBody) => {
     });
 };
 
+export const updateIPFSMetadata = async (JSONBody) => {
+    
+  let data = JSON.stringify(JSONBody);
+
+  const url = `https://api.pinata.cloud/pinning/hashMetadata`;
+  //making axios POST request to Pinata ⬇️
+  return axios
+    .put(url, JSONBody, {
+      headers: {
+        pinata_api_key: key,
+        pinata_secret_api_key: secret,
+        "Content-Type": "application/json",
+      },
+    })
+    .then(function (response) {
+      return {
+        success: true,
+        pinataURL:
+          "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash,
+      };
+    })
+    .catch(function (error) {
+      console.log(error);
+      return {
+        success: false,
+        message: error.message,
+      };
+    });
+};
+
 export const uploadFileToIPFS = async(file) => {
     const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
     //making axios POST request to Pinata ⬇️
